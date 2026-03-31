fa45ec4918bae4214b83dd1e7bf05ec85b443b20
sasahaa <104555378+sasahaa@users.noreply.github.com>
2026-03-31 20:59:16 +0300

    Update Hospital.java

--- src/Hospital.java
+++ src/Hospital.java
@@ -1,4 +1,46 @@
-package PACKAGE_NAME;
-
 public class Hospital {
+    private String name;
+    private String address;
+
+    public Hospital(String name, String adress){
+        this.name = name;
+        this.address = adress;
+    }
+
+    public void showHospital() {
+        System.out.println("Hospital: " + name);
+        System.out.println("Address: " + address);
+    }
+
+    // static nested class(вкладений клас)
+    static class Department{
+        private String name;
+        private int floor;
+
+        public Department(String name, int floor){
+            this.name = name;
+            this.floor = floor;
+        }
+
+        void showDepartment() {
+            System.out.println("Department: " + name + ", Floor: " + floor);
+        }
+    }
+
+    //inner class(внутрішній клас)
+    class Administrator{
+        private String fullName;
+        private String phoneNumber;
+        private String email;
+
+        Administrator(String name, String phoneNumber, String email){
+            this.fullName = name;
+            this.phoneNumber = phoneNumber;
+            this.email = email;
+        }
+
+        void manageHospital() {
+            System.out.println(fullName + " manages the hospital.");
+        }
+    }
 }
