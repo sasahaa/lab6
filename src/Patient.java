8131d65ab1ad87b623c2e11e950c1e30939dd1c5
sasahaa <104555378+sasahaa@users.noreply.github.com>
2026-03-31 20:59:32 +0300

    Update Patient.java

--- src/Patient.java
+++ src/Patient.java
@@ -1,4 +1,20 @@
-package PACKAGE_NAME;
-
 public class Patient {
+    private String name;
+    private String sex;
+    private String phoneNumber;
+    private String address;
+    private String email;
+
+    public  Patient(String name, String sex, String phoneNumber, String address, String email) {
+        this.name = name;
+        this.sex = sex;
+        this.phoneNumber = phoneNumber;
+        this.address = address;
+        this.email = email;
+
+    }
+    public String getName() {
+        return name;
+    }
 }
+
